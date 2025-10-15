b3d1a0c0139d2970b6fe60fa72a5e79019bde121
Production Services <macsnoeren@gmail.com>
2025-10-15 12:50:58 +0000

    Added refreshing of the page

--- htdocs/index.php
+++ htdocs/index.php
@@ -23,6 +23,7 @@ $total_assignments = $db->query("SELECT COUNT(*) FROM assignments")->fetchColumn
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="10">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Assignment Leaderboard</title>
     <style>
